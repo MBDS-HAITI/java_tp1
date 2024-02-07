f3d50bde8e0c9176f175b5280a8ab29a9433b562
Amosse Edouard <amosse.edouard@instant-system.com>
2024-02-07 20:28:31 +0100

    Ajout du modèle de données et des interfaces parking

--- src/ht/edu/fds/mbds/java/Main.java
+++ src/ht/edu/fds/mbds/java/Main.java
@@ -1,5 +1,8 @@
 package ht.edu.fds.mbds.java;
 
+import ht.edu.fds.mbds.java.controllers.Parking;
+import ht.edu.fds.mbds.java.entities.Car;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,7 +16,24 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        while (true) {
+        Parking parking = new Parking(2, 2, "Parking de l'Université", "Port-au-Prince");
+        System.out.println(parking);
+
+        Car car = new Car("1234", "Toyota", "Rouge", 4);
+        parking.addVoiture(car);
+        System.out.println(parking);
+
+        parking.addVoiture(new Car("1234", "Toyota", "Rouge", 4));
+        System.out.println(parking);
+
+        parking.removeVoiture(car);
+        System.out.println(parking);
+
+        parking.addVoiture(new Car("1234", "Toyota", "Rouge", 4));
+        System.out.println(parking);
+
+
+        /*while (true) {
             System.out.println("Entrez un mot");
             String name = readConsole();
             if (name.equals("exit")) {
@@ -22,6 +42,6 @@ public class Main {
             } else {
                 System.out.println("Vous avez entré: " + name);
             }
-        }
+        }*/
     }
 }
